7b79d18c061a1d18f40405473304d5b75d4be3b3
BillisC <26091373+BillisC@users.noreply.github.com>
2024-07-23 22:21:24 +0300

    update: Integrated CPP contents in header file
    Because Arduino IDE ignores .cpp files in other
    directories it had to be integrated in the header file.

--- Code/ML/tinyML.h
+++ Code/ML/tinyML.h
@@ -16,9 +16,26 @@
 #define ARENA_SIZE 5000
 Eloquent::TF::Sequential<TF_NUM_OPS, ARENA_SIZE> tf;
 
-void ml_init(void);
+void ml_init(void) {
+  Serial.println("__TINYML_INIT__");
 
-float ml_predict(float *input);
-unsigned int ml_predict_latency(void);
+  /* Initialize model loader and check for errors. */
+  while (!tf.begin(model).isOk()) {
+    Serial.println(tf.exception.toString());
+  }
+}
+
+float ml_predict(float *input) {
+  /* Predict and check for errors */
+  if (!tf.predict(input).isOk()) {
+    Serial.println(tf.exception.toString());
+  }
+
+  return tf.output(0);
+}
+
+unsigned int ml_predict_time(void) {
+    return tf.benchmark.microseconds();
+}
 
 #endif
