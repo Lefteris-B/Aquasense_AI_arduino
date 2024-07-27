4f8b709a59d5a4fa36f9de3bf8d1ceca9d931e78
BillisC <26091373+BillisC@users.noreply.github.com>
2024-07-27 11:38:53 +0300

    add: Scaling inputs, use in ino, and changed types
    I've added a continuous rotation function in .ino temporarily.
    The input are automatically scaled with the ML class and the types
    are now double oriented (which is bad for MCUs).

--- Code/PlantGuard/ML.cpp
+++ Code/PlantGuard/ML.cpp
@@ -24,14 +24,18 @@ bool ML::ml_init(void) {
   return !(fail);
 }
 
-uint8_t ML::ml_predict(uint8_t *input) {
+uint8_t ML::ml_predict(double *input) {
   if (fail == true) {
     return 0; // Don't move if something is off
   }
+  
+  /* Scale the input values */
+  float scaled[3] = {0};
+  input_scaler(input, scaled); 
 
   /* Normalize values with minmax algorithm */
   float norm[3] = {0};
-  minmax_norm(input, norm);
+  minmax_norm(scaled, norm);
 
   /* Predict and check for errors */
   if (!tf.predict(norm).isOk()) {
@@ -46,14 +50,20 @@ uint8_t ML::ml_predict(uint8_t *input) {
 }
 
 uint32_t ML::ml_predict_time(void) {
-    return tf.benchmark.microseconds();
+  return tf.benchmark.microseconds();
+}
+
+void ML::input_scaler(double *input, float *output) {
+  output[0] = round(10 * ((input[0] - 0) / (sizeof(double) - 0)));
+  output[1] = 0U; // Weather
+  output[2] = round(10 * 0); // Unknown limits
 }
 
 float ML::sigmoid_function(float input) {
   return (1 / (1 + exp(-input)));
 }
 
-void ML::minmax_norm(uint8_t *input, float *output) {
+void ML::minmax_norm(float *input, float *output) {
   output[0] = (input[0] - 0)/(10 - 0);  // Wind
   output[1] = (input[1] - 0)/(2 - 0);   // Weather
   output[2] = (input[2] - 0)/(10 - 0);  // Luminance
--- Code/PlantGuard/PlantGuard.ino
+++ Code/PlantGuard/PlantGuard.ino
@@ -1,20 +1,38 @@
 #include "Cover.hpp"
 #include "Valve.hpp"
+#include "ML.h"
 
 Valve valve;
+
+/* ML related */
+ML ml;
+double sample[3] = {0.0, 0.0, 0.0};
+
+/* Continuously rotate the protection 
+ * This should be moved in Cover.hpp 
+ */
+void cont_rotation() {
+  static bool rotate_forw = true;
+  if (rotate_forw) {
+    rotate_forw = !(cover.stepPositive());
+  }
+  else {
+    rotate_forw = !(cover.stepNegative());
+  }
+}
 
 void setup() {
   Serial.begin(115200);
 
   valve.attach(12);
-  
+
   cover.attachMotor(16, 17);
   cover.attachSwitch(32, 33);
 }
 
 void loop() {
-  // Go forth
-  while (!cover.stepPositive());
-  // And back
-  while (!cover.stepNegative());
+  // Go back and forth acoording to the model
+  if (ml.ml_predict(sample) == 1) {
+    cont_rotation();
+  }
 }
